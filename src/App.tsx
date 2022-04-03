b20e468884e946a453d7ef3e99c160f48fa9dac9
lijinhai <jinhai@carbonstop.net>
2022-04-03 23:11:08 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { registerRoute } from "workbox-routing";
-import { NetworkFirst } from "workbox-strategies";
-import { CacheableResponsePlugin } from "workbox-cacheable-response";
+// import { registerRoute } from "workbox-routing";
+// import { NetworkFirst } from "workbox-strategies";
+// import { CacheableResponsePlugin } from "workbox-cacheable-response";
+// import { precacheAndRoute } from "workbox-precaching";
+
+// Use with precache injection
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [title, getTitle] = useState<"">("");
   useEffect(() => {
-    registerRoute(
-      // Check to see if the request is a navigation to a new page
-      ({ request }) => request.mode === "navigate",
-      // Use a Network First caching strategy
-      new NetworkFirst({
-        // Put all cached files in a cache named 'pages'
-        cacheName: "pages",
-        plugins: [
-          // Ensure that only requests that result in a 200 status are cached
-          new CacheableResponsePlugin({
-            statuses: [200],
-          }),
-        ],
-      })
-    );
-    if ("serviceWorker" in navigator) {
-      // Use the window load event to keep the page load performant
-      window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/service-worker.js");
-      });
-    }
+    // precacheAndRoute(self.__WB_MANIFEST);
+    // registerRoute(
+    //   // Check to see if the request is a navigation to a new page
+    //   ({ request }) => request.mode === "navigate",
+    //   // Use a Network First caching strategy
+    //   new NetworkFirst({
+    //     // Put all cached files in a cache named 'pages'
+    //     cacheName: "pages",
+    //     plugins: [
+    //       // Ensure that only requests that result in a 200 status are cached
+    //       new CacheableResponsePlugin({
+    //         statuses: [200],
+    //       }),
+    //     ],
+    //   })
+    // );
+    // if ("serviceWorker" in navigator) {
+    //   // Use the window load event to keep the page load performant
+    //   window.addEventListener("load", () => {
+    //     navigator.serviceWorker.register("/service-worker.js");
+    //   });
+    // }
     fetch("https://yd-cloudflare-server-ts.9760414764447.workers.dev/")
       .then((response: Response) => response?.json())
       .then((data) => {
